01678f09c810f2427b096d85e00eafd2ab4860c4
Shahwar Spall <shevy.spall@gmail.com>
2020-01-28 23:48:05 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-Insert INTO bears(name,age) VALUES ('Mr.Chocolate','3');
-Insert INTO bears(name,age) VALUES ('Rowdy','5');
-Insert INTO bears(name,age) VALUES ('Tabitha','32');
-Insert INTO bears(name,age) VALUES ('Sergeant Brown','6');
-Insert INTO bears(name,age) VALUES ('Melissa','4');
-Insert INTO bears(name,age) VALUES ('Grinch','7');
-Insert INTO bears(name,age) VALUES ('Wendy','9');
-Insert INTO bears(name,age,alive) VALUES ('NULL','9','killed');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr Chocolate", 1, "male", "gray", "nervous", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 2, "female", "white", "calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 3, "male", "black", "agressive", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 4, "female", "dark", "lazy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 5, "male", "brown", "nervous", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 6, "male", "dark brown", "naive", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 7, "female", "intense", "friendly", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 8, "male", "navy blue", "agressive", 0);
