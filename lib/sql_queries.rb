16ec2d1ad963dc3f572aafe7d749552011f54b40
Shahwar Spall <shevy.spall@gmail.com>
2020-01-31 00:42:14 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,11 +19,11 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT color FROM bears "
+  "SELECT color, COUNT(*) FROM bears WHERE color = 'dark brown'"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
+  "SELECT COUNT(*) "
 end
 
 def selects_bear_that_killed_Tim
